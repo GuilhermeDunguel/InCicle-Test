4daccfc1454bdd2817f6b9f386c614dcbd8d84bb
Guilherme Dunguel <gdunguel2012@gmail.com>
2022-05-27 15:42:34 -0300

    Create project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header'
+import Main from './components/Main/Main'
+
+import './global.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+    </div>
+  )
+}
+
+export default App
